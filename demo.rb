4c2d4373bc98d70b43bb9cb3446800267cc52a1e
Ruari Day <ruariday@gmail.com>
2020-09-30 12:12:55 +0000

    Automatically backed up by Learn

---
+++ demo.rb
@@ -0,0 +1,9 @@
+#write if...end.
+
+run_code_inside = true;
+puts "Code before if...end";
+ if run_code_inside;
+   puts "code inside";
+ end
+ puts "Code after if...end"
+ 
---
+++ greeting.rb
@@ -0,0 +1,13 @@
+def greeting
+  puts "Hello Wolrd!"
+end
+
+def say_geeting_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+
+say_geeting_five_times
